bac8dc9f44f533b476eacd90319e7703ae6e578a
Subhranshu Das <subhranshu@Subhranshus-MacBook-Pro.local>
2022-05-24 15:43:52 +0530

    chore(restapi): add more sample tests

--- packages/restapi/src/lib/restapi.spec.ts
+++ packages/restapi/src/lib/restapi.spec.ts
@@ -1,7 +1,17 @@
-import { restapi } from './restapi';
+import { restapi, callApi } from './restapi';
 
 describe('restapi', () => {
   it('should work', () => {
     expect(restapi()).toEqual('restapi');
+  });
+
+  describe('callApi', () => {
+    it('should give data for a supported network', () => {
+      expect(callApi(1)).toBeDefined();
+    });
+  
+    it('should be not defined for an un supported network', () => {
+      expect(callApi(100)).not.toBeDefined();
+    });
   });
 });
--- packages/restapi/src/lib/restapi.ts
+++ packages/restapi/src/lib/restapi.ts
@@ -1,3 +1,10 @@
+import CONFIG from "./config";
+
 export function restapi(): string {
   return 'restapi';
+}
+
+export function callApi(chainId: number): string {
+  const { API_BASE_URL } = CONFIG[chainId] || {};
+  return API_BASE_URL;
 }
