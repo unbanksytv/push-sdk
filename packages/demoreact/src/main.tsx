fccbf353bf44b4941296e631f5b12636893426a6
Subhranshu Das <subhranshu@Subhranshus-MacBook-Pro.local>
2022-05-25 15:12:05 +0530

    chore(demoreact): add demo react package placeholder
    sandbox react web app to test other sdk parts

---
+++ packages/demoreact/src/main.tsx
@@ -0,0 +1,13 @@
+import { StrictMode } from 'react';
+import { render } from 'react-dom';
+import { BrowserRouter } from 'react-router-dom';
+import App from './app/app';
+
+const container = document.getElementById('root');
+render((
+  <StrictMode>
+     <BrowserRouter>
+      <App />
+    </BrowserRouter>
+  </StrictMode>
+), container);
